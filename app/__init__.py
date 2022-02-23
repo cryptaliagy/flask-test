c69b66a0bf0f0d9912535d34404ab8977de12ec7
Natalia Maximo <natalia.maximo@uottawa.ca>
2022-02-23 01:33:07 -0500

    change logging and task parameters

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ import logging
 from flask import (
     Flask,
     jsonify,
+    request,
 )
 from flask_apscheduler import (
     APScheduler
@@ -13,17 +14,18 @@ class Config:
     SECRET_KEY = secrets.token_hex(16)
     ENV = 'development'
     SCHEDULER_API_ENABLED = True
+    SCHEDULER_ALWAYS_START = True
 
 
 cron = APScheduler()
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
 
     cron.init_app(app)
+
+    app.logger.setLevel(logging.DEBUG)
 
     @app.route('/heartbeat')
     def heartbeat():
@@ -32,9 +34,15 @@ def create_app():
             msg='Service is up!'
         )
 
-    @cron.task('interval', id='test', seconds=10)
+    @cron.task('interval', id='some_test', seconds=10, misfire_grace_time=300)
     def task():
-        logger.debug('Firing off task!')
+        app.logger.debug('Firing off task!')
+
+    @app.before_request
+    def _():
+        app.logger.debug('Hit route %s', request.url)
+        if not cron.running:
+            cron.start()
 
     cron.start()
 
